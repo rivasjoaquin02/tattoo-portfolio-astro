51bc0961cb7352f9943a020847dbe7949953e4d3
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2023-12-15 10:10:16 -0500

    added section for services and images optimized

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,33 @@
 export const images = [
-	{ name: "img1", src: "img1.jpg" },
-	{ name: "img2", src: "img2.png" },
-	{ name: "img3", src: "img3.png" },
-	{ name: "img4", src: "img4.png" },
-	{ name: "img5", src: "img5.png" },
-	{ name: "img6", src: "img6.png" },
+	{ name: "img1", src: "img1.webp" },
+	{ name: "img2", src: "img2.webp" },
+	{ name: "img3", src: "img3.webp" },
+	{ name: "img4", src: "img4.webp" },
+	{ name: "img5", src: "img5.webp" },
+	{ name: "img6", src: "img6.webp" },
+];
+
+export const LUMNA_PICTURE = "person.webp";
+export const SEPARATOR = "separator.svg";
+export const DESCRIPTION = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quia
+repudiandae vero, saepe nulla perspiciatis architecto, esse officiis
+laboriosam veritatis expedita repellendus veniam aperiam, fugit
+autem nisi labore culpa quis!`;
+
+export const SERVICES = [
+	{
+		name: "service1",
+		desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+		img: "img2.webp",
+	},
+	{
+		name: "service1",
+		desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+		img: "img2.webp",
+	},
+	{
+		name: "service1",
+		desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+		img: "img2.webp",
+	},
 ];
